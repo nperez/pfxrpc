b8d252e317742b44ecea5d247302ef8317205caf
nperez <nperez@cpan.org>
2009-04-02 21:33:11 +0000

    r244@eeepc:  nicholas | 2009-04-02 16:32:51 -0500  Simplify the filter to only work with PFX::Nodes. Build the stack elsewhere

--- lib/POE/Filter/XML/RPC.pm
+++ lib/POE/Filter/XML/RPC.pm
@@ -1,158 +1,256 @@
-package POE::Filter::XML::RPC;
+package POE::Filter::XML::RPC::Guts;
 
-use warnings;
-use strict;
+use POE::Filter::XML::RPC::Request;
+use POE::Filter::XML::RPC::Response;
+use POE::Filter::XML::RPC::ValueFactory;
+use POE::Filter::XML::RPC::Fault;
 
-use POE;
-use POE::Filter::Stackable;
-use POE::Filter::Stream;
-use POE::Filter::HTTPD;
-use POE::Filter::XML;
-use POE::Filter::XML::RPC::Guts;
-use POE::Filter::XML::RPC::HTTPShim;
-use Exporter;
+use POE::Filter::XML::Node;
 
-use base('POE::Filter::Stackable', 'Exporter');
+use base('POE::Filter');
 
 use constant
 {
-	'BUFFER'	=> 0,
-	'HTTP'		=> 1,
-	'TCP'		=> 2,
+	BUFFER => 0,
 };
 
-our $VERSION = '0.01';
-our @EXPORT = 
-(
-	'TCP', 
-	'HTTP', 
-	'SERVER_MODE',
-	'CLIENT_MODE',
-);
-
 sub new()
-{	
-	my $class = shift(@_);
-	
-	if(@_ & 1)
-	{
-		Carp::confess('Please provide an even number of arguments');
-	}
-
-	my $config = {};
-	
-	while($#_ != -1)
-	{
-		my $key = lc(shift(@_));
-		my $val = shift(@_);
-		$config->{$key} = $val;
-	}
-
+{
+	my $class = shift;
 	my $self = [];
+	
 	$self->[+BUFFER] = [];
 
-	bless($self, $class);
+	return bless($self, $class);
+}
 
-	unless($config->{'transport'})
+sub get_one_start()
+{
+	my ($self, $raw) = @_;
+	if(@{$self->[+BUFFER]})
 	{
-		$config->{'transport'} = +HTTP;
-	}
-	
-	if($config->{'transport'} == +HTTP)
-	{
-		unless(exists($config->{'mode'}))
-		{
-			Carp::confess('Client/Server mode specification is required!');
-		}
-
-		$self->push(POE::Filter::HTTPD->new());
-		
-		$self->push
-		(
-			POE::Filter::XML::RPC::HTTPShim->new
-			(
-				'Mode'			=> $config->{'mode'},
-				'URI'			=> $config->{'uri'},
-				'HostHeader'	=> $config->{'hostheader'},
-				'UserAgent'		=> $config->{'useragent'},
-				'ServerHeader'	=> $config->{'serverheader'},
-			)
-		);
-	
-	} elsif($config->{'transport'} == +TCP) {
-		
-		$self->push(POE::Filter::Stream->new());
+		push(@{$self->[+BUFFER]}, @$raw);
 	
 	} else {
 
-		Carp::confess('Unknown transport type!');
+		$self->[+BUFFER] = $raw;
+	}
+}
+
+sub get_one()
+{
+	my $self = shift(@_);
+	
+	my $node = shift(@{$self->[+BUFFER]});
+	
+
+	if(defined($node))
+	{
+		if($node->nodeName() eq 'methodCall')
+		{
+			my $children = $node->getChildrenHash();
+			if(exists($children->{'methodName'}))
+			{
+				my $data = $children->{'methodName'}->textContent();
+				if(!defined($data) and !length($data))
+				{
+					return 
+					[
+						POE::Filter::XML::RPC::Fault->new
+						(
+							102,
+							'Malformed XML-RPC: No methodName data defined'
+						)
+					];
+				}
+			
+			} else {
+
+				return
+				[
+					POE::Filter::XML::RPC::Fault->new
+					(
+						103,
+						'Malformed XML-RPC: No methodName child tag present'
+					)
+				];
+			}
+			
+			# params are optional, but let's be consistent for the
+			# Request code's sake.
+
+			if(!exists($children->{'params'}))
+			{
+				$node->appendChild('params');
+			
+			} else {
+
+				my $params = $children->{'params'}->getChildrenByTagName('*');
+
+				foreach my $param (@$params)
+				{
+					my $value = $param->getSingleChildByTagName('value');
+					
+					if(!defined($value))
+					{
+						return
+						[
+							POE::Filter::XML::RPC::Fault->new
+							(
+								110,
+								'Malformed XML-RPC: No value tag within param'
+							)
+						];
+					}
+				}
+			}
+					
+			bless($node, 'POE::Filter::XML::RPC::Request');
+			return [$node];
+	
+		} elsif ($node->name() eq 'methodResponse') {
+			
+			my $children = $node->getChildrenHash();
+
+			if(!exists($children->{'params'}) and 
+				!exists($children->{'fault'}))
+			{
+				return
+				[
+					POE::Filter::XML::RPC::Fault->new
+					(
+						104,
+						'Malformed XML-RPC: Response does not contain ' .	
+						'parameters or a fault object'
+					)
+				]
+			
+			} elsif(exists($children->{'params'})) {
+					
+				my $params = $children->{'params'}->getChildrenByTagName('*');
+
+				if(!@{$params})
+				{
+					return
+					[
+						POE::Filter::XML::RPC::Fault->new
+						(
+							105,
+							'Malformed XML-RPC: Return parameters does ' .
+							'not contain any param children'
+						)
+					];
+				
+				} 
+
+				foreach my $param (@$params)
+				{
+					my $value = $param->getSingleChildByTagName('value');
+
+					if($param->nodeName() ne 'param')
+					{
+						return
+						[
+							POE::Filter::XML::RPC::Fault->new
+							(
+								108,
+								'Malformed XML-RPC: Params object ' .
+								'contains children other than param'
+							)
+						];
+					}
+
+					if(!defined($value))
+					{
+						return
+						[
+							POE::Filter::XML::RPC::Fault->new
+							(
+								109,
+								'Malformed XML-RPC: Param child does '.
+								'not contain a value object'
+							)
+						];
+					}
+				}
+		
+			} elsif(exists($children->{'fault'})) {
+
+				my $fault = $children->{'fault'};
+				my $value = $fault->getSingleChildByTagName('value');
+
+				my $struct = 
+					POE::Filter::XML::RPC::ValueFactory::value_process
+					(
+						$value
+					);
+
+				if(!$struct->isa('POE::Filter::XML::RPC::Value::Struct'))
+				{
+					return
+					[
+						POE::Filter::XML::RPC::Fault->new
+						(
+							106,
+							'Malformed XML-RPC: Fault value is not a ' .
+							'valid struct object'
+						)
+					];
+				
+				} 
+				elsif(!defined($struct->get_member('faultCode')) or
+					!defined($struct->get_member('faultString')))
+				{
+					return
+					[
+						POE::Filter::XML::RPC::Fault->new
+						(
+							107,
+							'Malformed XML-RPC: Fault value does not ' . 
+							'contain either a fault code or fault string'
+						)
+					];
+				}
+
+				bless($fault, 'POE::Filter::XML::RPC::Fault');
+                return [$fault];
+			}
+				
+			bless($node, 'POE::Filter::XML::RPC::Response');
+			return [$node];
+		
+		} else {
+			
+			warn $node->toString();
+
+			return 
+			[
+				POE::Filter::XML::RPC::Fault->new
+				( 
+					101, 
+					'Malformed XML-RPC: Top level node is not valid'
+				)
+			];
+		}
+	
+	} else {
+
+		return [];
+	}
+}
+
+sub put()
+{
+	my ($self, $nodes) = @_;
+	
+	my $ret = [];
+
+	foreach my $node (@$nodes)
+	{
+		push(@$ret, bless($node, 'POE::Filter::XML::Node'));
 	}
 	
-	#turn off streaming documents
-	$self->push(POE::Filter::XML->new('NotStreaming' => 1));
-	$self->push(POE::Filter::XML::RPC::Guts->new());
-	
-	return $self;
+	return $ret;
 }
 
 1;
-__END__
-=head1 NAME
-
-=head1 VERSION
-
-Version 0.01
-
-=head1 SYNOPSIS
-
-=head1 AUTHOR
-
-Nicholas R. Perez, C<< <nperez at cpan.org> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to
-C<bug-poe-filter-xml-rpc at rt.cpan.org>, or through the web interface at
-L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=POE-Filter-XML-RPC>.
-I will be notified, and then you'll automatically be notified of progress on
-your bug as I make changes.
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc POE::Filter::XML::RPC
-
-You can also look for information at:
-
-=over 4
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/POE-Filter-XML-RPC>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/POE-Filter-XML-RPC>
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=POE-Filter-XML-RPC>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/POE-Filter-XML-RPC>
-
-=back
-
-=head1 ACKNOWLEDGEMENTS
-
-=head1 COPYRIGHT & LICENSE
-
-Copyright 2007 Nicholas R. Perez, all rights reserved.
-
-This program is released under the following license: GPL
-
-=cut
-
-1; # End of POE::Filter::XML::RPC
